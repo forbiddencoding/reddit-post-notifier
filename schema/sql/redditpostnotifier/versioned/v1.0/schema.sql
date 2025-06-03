f87844490aca0b8223350f9171190fae60c13484
forbiddencoding <ed.is.coding@gmail.com>
2025-06-03 21:51:06 +0200

    Initial SQL schema

---
+++ schema/sql/redditpostnotifier/versioned/v1.0/schema.sql
@@ -0,0 +1,37 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS configuration
+(
+    id              BIGINT    NOT NULL,
+    keyword         TEXT      NOT NULL,
+    created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS subreddit_configuration
+(
+    id                 BIGINT    NOT NULL,
+    configuration_id   BIGINT    NOT NULL,
+    subreddit          TEXT      NOT NULL,
+    include_nsfw       BOOLEAN   NOT NULL DEFAULT FALSE,
+    sort               TEXT      NOT NULL DEFAULT 'new',
+    restrict_subreddit BOOLEAN   NOT NULL DEFAULT TRUE,
+    created_at         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated_at    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (configuration_id) REFERENCES configuration (id) ON DELETE CASCADE
+);
+
+CREATE INDEX IF NOT EXISTS idx_subreddit_configuration_configuration_id
+    ON subreddit_configuration (configuration_id);
+
+CREATE TABLE IF NOT EXISTS subreddit_configuration_state
+(
+    subreddit_configuration_id BIGINT UNIQUE NOT NULL,
+    before                     TEXT          NOT NULL,
+    last_updated_at            TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (subreddit_configuration_id) REFERENCES subreddit_configuration (id) ON DELETE CASCADE
+);
+
+COMMIT;
